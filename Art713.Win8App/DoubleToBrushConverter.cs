43850adc29e78b61fb5adbb5adfc19a1228c877b
makaveli713 <makaveli713@yandex.ua>
2013-04-07 00:53:51 +0300

    #9.
    Добавил класс DoubleToBrushConverter, реализующий интерфейс IValueConverter.
    Связал textbox & slider, указав значению Converter класс DoubleToBrushConverter,
    предварительно добавив этот класс в ресурсы страницы.

---
+++ Art713.Win8App/DoubleToBrushConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Art713.Win8App
+{
+    class DoubleToBrushConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is double)
+            {
+                var doubleVal = (double) value;
+                if (doubleVal<40)                
+                    return new SolidColorBrush(Colors.Green);
+                if (doubleVal > 40 && doubleVal < 80)
+                    return new SolidColorBrush(Colors.Yellow);
+                return new SolidColorBrush(Colors.Red);
+            }
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
