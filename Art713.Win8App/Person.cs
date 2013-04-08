d380c699a30405059c3c7fbf38f988849873ff59
makaveli713 <makaveli713@yandex.ua>
2013-04-08 23:23:58 +0300

    #10. Implemented INotifyPropertyChanged. Теперь класс Person уведомляет о изменении своих свойств!

--- Art713.Win8App/Person.cs
+++ Art713.Win8App/Person.cs
@@ -1,9 +1,26 @@
-﻿namespace Art713.Win8App
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Art713.Win8App.Annotations;
+
+namespace Art713.Win8App
 {
-    class Person
+    class Person : INotifyPropertyChanged
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+
+        public string FirstName { get { return _firstName; } set { _firstName = value; OnPropertyChanged("FirstName"); } }
+        public string LastName { get { return _lastName; } set { _lastName = value; OnPropertyChanged("LastName"); } }
+        public string Email { get { return _email; } set { _email = value; OnPropertyChanged("Email"); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) 
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
