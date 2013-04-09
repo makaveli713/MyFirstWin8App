cd76fbc917286e5571a28a1c52fda6065b1b9a4d
makaveli713 <makaveli713@yandex.ua>
2013-04-09 21:25:17 +0300

    #23.
    Добавил класс PersonGroup.
    Элементы будут группироваться в соответствии с должностями из этого класса.

---
+++ ControlsApp/PersonGroup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ControlsApp
+{
+    class PersonGroup
+    {
+        public string GroupName { get; set; }
+        public ObservableCollection<Person> Persons { get; set; }
+
+        public PersonGroup()
+        {
+            Persons = new ObservableCollection<Person>();
+        }
+    }
+}
