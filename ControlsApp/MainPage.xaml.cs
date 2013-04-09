787bfe62537b9cd6c967c6c4741846b641492ec4
makaveli713 <makaveli713@yandex.ua>
2013-04-09 20:59:38 +0300

    #21.
    Добавил коллецию типа ObservableCollection<Person>
    и связал её с GridView.

--- ControlsApp/MainPage.xaml.cs
+++ ControlsApp/MainPage.xaml.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+﻿using System.Collections.ObjectModel;
 using Windows.UI.Xaml.Navigation;
 
 // Шаблон элемента пустой страницы задокументирован по адресу http://go.microsoft.com/fwlink/?LinkId=234238
@@ -19,11 +8,36 @@ namespace ControlsApp
     /// <summary>
     /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
+        private ObservableCollection<Person> _persons;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            _persons = new ObservableCollection<Person>
+                {
+                    new Person {LastName = "Boyko", FirstName = "Dima", Age = 21},
+                    new Person {LastName = "Senatorova", FirstName = "Olga", Age = 21},
+                    new Person {LastName = "Trubitsyn", FirstName = "Artem", Age = 22},
+                    new Person {LastName = "Dihtyarenko", FirstName = "Vasiliy", Age = 71}, 
+                    new Person {LastName = "Dihtyarenko", FirstName = "Elena", Age = 65},
+                    new Person {LastName = "Gustin", FirstName = "Art", Age = 72},
+                    new Person {LastName = "Huggin", FirstName = "Pat", Age = 65},
+                    new Person {LastName = "Gregor", FirstName = "Luc", Age = 25},
+                    new Person {LastName = "Gustin", FirstName = "Chris", Age = 45},
+                    new Person {LastName = "Gustin", FirstName = "Tammy", Age = 50},
+                    new Person {LastName = "Dihtyarenko", FirstName = "Vasiliy", Age = 71}, 
+                    new Person {LastName = "Dihtyarenko", FirstName = "Elena", Age = 65},
+                    new Person {LastName = "Rud", FirstName = "Ivan", Age = 21},
+                    new Person {LastName = "Trubitsyna", FirstName = "Anna", Age = 15},
+                    new Person {LastName = "Trubitsyna", FirstName = "Irina", Age = 44},
+                    new Person {LastName = "Trubitsyn", FirstName = "Vladimir", Age = 45},
+                    new Person {LastName = "Gregor", FirstName = "Luc", Age = 25}                    
+                };
+
+            gvMain.ItemsSource = _persons;
         }
 
         /// <summary>
