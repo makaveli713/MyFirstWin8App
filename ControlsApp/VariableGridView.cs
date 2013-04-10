e87c714675df27b5eaa827195ce67eece2fc62ef
makaveli713 <makaveli713@yandex.ua>
2013-04-10 10:25:35 +0300

    #24. Установил свойства VariableSizedWrapGrid.ColumnSpanProperty и
    VariableSizedWrapGrid.RowSpanProperty для каждого элемента в соответствии
    с данными, определенными в объектах класса Person

---
+++ ControlsApp/VariableGridView.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ControlsApp
+{
+    public class VariableGridView : GridView
+    {
+        protected override void PrepareContainerForItemOverride(Windows.UI.Xaml.DependencyObject element, object item)
+        {
+            var personItem = item as Person;
+            if (personItem != null)
+            {
+                element.SetValue(VariableSizedWrapGrid.ColumnSpanProperty, personItem.HorizontalSize);
+                element.SetValue(VariableSizedWrapGrid.RowSpanProperty, personItem.VerticalSize);
+            }
+            base.PrepareContainerForItemOverride(element, item);
+        }
+    }
+}
